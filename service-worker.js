caf4dac07c4cdd90b27a4d0f5a25122de426d6a5
gopalajaykrishnan <gopalajaykrishnan@gmail.com>
2025-08-21 23:52:26 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,45 @@
-self.addEventListener('install', (event) => {
+const CACHE_NAME = "app-cache-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// Install
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open('app-cache').then((cache) => {
-      return cache.addAll(['./index.html', './manifest.json']);
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Caching app files");
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
+// Activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log("Clearing old cache:", key);
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
